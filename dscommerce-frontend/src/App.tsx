568599ab3a7a9e8fb4094cf602d9315d1adeadf1
Samuel Bartolomedi <samuelbartolomedi1994@gmail.com>
2026-09-04 15:22:20 -0300

    creating components

--- dscommerce-frontend/src/App.tsx
+++ dscommerce-frontend/src/App.tsx
@@ -1,63 +1,22 @@
 import './App.css'
-import computerImage from './assets/computer.png'
-import cartImage from './assets/cart.svg'
+import ButtonPrimary from './components/ButtonPrimary';
+import ButtonSecondary from './components/ButtonSecondary';
+import HeaderClient from './components/HeaderClient';
+import ProductDetailsCard from './components/ProductDetailsCard';
 
 function App() {
   return (
     <>
-
-      <header className="dsc-header-client">
-        <nav className="dsc-container">
-          <h1>DSCommerce</h1>
-          <div className="dsc-navbar-right">
-            <div className="dsc-menu-items-container">
-              <div className="dsc-menu-item">
-                <img src={cartImage} alt="Carrinho de compras" />
-              </div>
-            </div>
-            <a href="#">Entrar</a>
-          </div>
-        </nav>
-      </header>
+      <HeaderClient />
       <main>
         <section id="product-details-section" className="dsc-container">
-          <div className="dsc-card dsc-mb20">
-            <div className="dsc-product-details-top dsc-line-bottom">
-              <img src={computerImage} alt="Computador" />
-            </div>
-            <div className="dsc-product-details-bottom">
-              <h3>R$ 5000,00</h3>
-              <h4>Computador Gamer XT</h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-              <div className="dsc-category-container">
-                <div className="dsc-category">
-                  Eletrônicos
-                </div>
-                <div className="dsc-category">
-                  Computadores
-                </div>
-              </div>
-            </div>
-          </div>
+         <ProductDetailsCard />
           <div className="dsc-btn-page-container">
-            <div className="dsc-btn dsc-btn-blue">
-              Comprar
-            </div>
-            <div className="dsc-btn dsc-btn-white">
-              Início
-            </div>
+            <ButtonPrimary />
+            <ButtonSecondary />
           </div>
         </section>
       </main>
-
     </>
   );
 }
---
+++ dscommerce-frontend/src/components/ButtonSecondary/index.tsx
@@ -0,0 +1,9 @@
+import './styles.css'
+
+export default function ButtonSecondary() {
+    return (
+        <div className="dsc-btn dsc-btn-white">
+            Início
+        </div>
+    )
+}
